0e0f9401f29b3e9248e45107a23b4c980a279255
Luke Fromhold <luke@fromhold.io>
2019-02-18 17:01:55 +1100

    Module initial commit

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # silverstripe-commonancestor
+
+A small utility class that accepts an array of class name, compares their class ancestories, and identifies their closest common ancestor class.
+
+Class is pretty well commented, jumping into it should answer any initial questions.
+
+## Requirements
+
+SilverStripe 4
+
+## Installation
+
+`composer require fromholdio/silverstripe-commonancestor`
+
+## (Contrived) Example
+
+Assuming a page class hierarchy of:
+
+* SiteTree
+    * Page
+     * BlogHolder
+     * BlogPost
+       * ArticlePost
+       * VideoPost
+
+```php
+$exampleOne = [VideoPost::class, ArticlePost::class];
+$resultOne = CommonAncestor::get_closest($exampleOne);
+// returns 'BlogPost'
+
+$exampleTwo = [VideoPost::class, BlogPost::class];
+$resultTwo = CommonAncestor::get_closest($exampleTwo);
+// returns 'BlogPost'
+
+$exampleThree = [VideoPost::class, BlogHolder::class];
+$resultThree = CommonAncestor::get_closest($exampleThree);
+// returns 'Page'
+```
+
+The `get_closest` function also accepts a second argument, which allows you to include/exclude classes that have tables (`$tablesOnly`; set to `false` by default).
---
+++ src/CommonAncestor.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Fromholdio\CommonAncestor;
+
+use SilverStripe\Core\ClassInfo;
+
+class CommonAncestor
+{
+    /**
+     * Accepts an array of class names, whose ancestries will be compared, to identify
+     * their closest common ancestor class.
+     *
+     * @param array $classes Array of class names to find a common ancestor for.
+     * @param boolean $tablesOnly Only include classes that have a table in the database.
+     *
+     * @return string Common ancestor class name.
+     */
+    public static function get_closest(array $classes, $tablesOnly = false)
+    {
+        if (empty($classes)) {
+            throw new \InvalidArgumentException(
+                'You must provide at least one class in your $classes array.'
+            );
+        }
+
+        foreach ($classes as $class) {
+            if (!ClassInfo::exists($class)) {
+                throw new \InvalidArgumentException(
+                    'Invalid class provided to CommonAncestor::get_closest().'
+                    . $class . ' does not exist.'
+                );
+            }
+        }
+
+        if (count($classes) === 1) {
+            return reset($classes);
+        }
+
+        /*
+         * Loop over each class, making a list of all ancestor classes,
+         * building a count of how many times each class appears in ancestry.
+         */
+        $ancestors = [];
+        foreach ($classes as $class) {
+            $myAncestors = ClassInfo::ancestry($class, $tablesOnly);
+
+            foreach ($myAncestors as $myAncestor) {
+                if (isset($ancestors[$myAncestor])) {
+                    $ancestors[$myAncestor] = $ancestors[$myAncestor] + 1;
+                }
+                else {
+                    $ancestors[$myAncestor] = 1;
+                }
+            }
+        }
+
+        /*
+         * Ancestor classes are common if their count is equal to the total supplied classes
+         * (these are the classes that all supplied classes share in their ancestry trees)
+         */
+        foreach ($ancestors as $ancestorClass => $ancestorCount) {
+
+            // Remove classes that do not appear in ancestry of all supplied classes
+            if ($ancestorCount !== count($classes)) {
+                unset($ancestors[$ancestorClass]);
+            }
+        }
+
+        /*
+         * If only one ancestor class remains, return that value
+         */
+        if (count($ancestors) === 1) {
+            return current(array_flip($ancestors));
+        }
+
+        /*
+         * If more than one ancestor class, determine which is closest in hierarchy
+         * to the supplied classes. To do this we simply use the first of the supplied
+         * classes and walk through its ancestry until we find the first match.
+         */
+        $myAncestors = array_reverse(ClassInfo::ancestry(reset($classes)), $tablesOnly);
+        foreach ($myAncestors as $myAncestor) {
+            if (array_key_exists($myAncestor, $ancestors)) {
+                return $myAncestor;
+            }
+        }
+    }
+}
